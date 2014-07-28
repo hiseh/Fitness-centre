0843d928d7deb0dee435b2d0db03ed8ae2771f91
Hiseh <hisehyin@gmail.com>
2014-07-28 22:28:28 +0800

    first commit

---
+++ Fitness centre/Fitness centreTests/Fitness_centreTests.m
@@ -0,0 +1,34 @@
+//
+//  Fitness_centreTests.m
+//  Fitness centreTests
+//
+//  Created by hiseh yin on 14-7-28.
+//  Copyright (c) 2014年 hiseh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Fitness_centreTests : XCTestCase
+
+@end
+
+@implementation Fitness_centreTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
