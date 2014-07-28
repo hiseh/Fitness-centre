930df6f2e957a1041096211dde72a65fbb64eda1
Hiseh <hisehyin@gmail.com>
2014-07-28 22:22:35 +0800

    new project

---
+++ xcode create git repository on my mac/xcode create git repository on my macTests/xcode_create_git_repository_on_my_macTests.m
@@ -0,0 +1,34 @@
+//
+//  xcode_create_git_repository_on_my_macTests.m
+//  xcode create git repository on my macTests
+//
+//  Created by hiseh yin on 14-7-28.
+//  Copyright (c) 2014年 hiseh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xcode_create_git_repository_on_my_macTests : XCTestCase
+
+@end
+
+@implementation xcode_create_git_repository_on_my_macTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
